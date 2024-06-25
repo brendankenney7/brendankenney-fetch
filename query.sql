0bf9e53c5f86f25f512b6b5083c3e46ef399baa2
brendankenney7 <brendankenney07@gmail.com>
2024-06-24 22:06:15 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,18 +1,15 @@
 -- Written in mssql
 select
     rd.brandCode,
-    b.name as brandName,
     count(rd.partnerItemId) as transactionsLast6MonthUser,
     sum(rd.finalPrice) as totalSpendLast6MonthUser
 from receipt_detail rd
-    left join brand b
-    on b.brandCode = rd.brandCode
     join receipt r
     on r._id = rd.receiptId
     join user u
     on u._id = r.userId
 where u.created_date >= DATEADD('month', -6, GETDATE())
-group by rd.brandCode, b.name
+group by rd.brandCode
 order by totalSpendLast6MonthUser desc
 
 -- This query will list all brands available in receipt scan data and their corresponding spend and transaction count from users created within the last 6 months.
@@ -24,7 +21,7 @@ order by totalSpendLast6MonthUser desc
 ---    If I wanted to pull just the top brand, I could start the query with 'select top 1 rd.brand code, ...' and end the code with 'order by [metric] desc'
 ---    If I wanted to pull the top brand for both transactions and spend, I could use CTEs to pull the top spend brand, then the top transaction brand, then union the metrics together in the parent select statement.
 
--- The user data set was corrupted, so I could not view the data set. 
+-- The user data set was corrupted, so I could not view the data set. The join and condition were created based on assumptions from the information sheet.
 
 -- includes items where needsFetchReview = TRUE
 -- includes inactive users created within the last 6 months
